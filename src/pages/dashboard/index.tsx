91a108da22bc488d7737b2c6818df7d8cc7863f8
Igor Mazetti de Azevedo <iigormazetti@hotmail.com>
2023-05-25 15:36:33 -0300

    feat: created search input

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,11 +1,23 @@
 import Header from "@/components/Header";
+import { Box, Flex, Input } from "@chakra-ui/react";
+import { GoSearch } from "react-icons/go";
 import React from "react";
 
 export default function Dashboard() {
   return (
     <div>
       <Header companyName="Scan Borba" />
-      algo
+      <Flex align="center" justify="center" h="120px">
+        <Input placeholder="Busque um produto pelo nome" w="420px" />
+        <Box
+          ml="-40px"
+          h="42px"
+          _hover={{ cursor: "pointer", transform: "scale(1.1)" }}
+          zIndex={10}
+        >
+          <GoSearch size={28} />
+        </Box>
+      </Flex>
     </div>
   );
 }
